934f8856ae2a31e672379e8bc76b769085fd9f92
riwobeto <rpwobeto@yahoo.com.br>
2021-11-12 13:51:43 -0300

    Desafio 2

---
+++ src/main/java/com/trilha/back/financys/Category.java
@@ -0,0 +1,48 @@
+package com.trilha.back.financys;
+
+public class Category {
+    private long id;
+    private String name;
+    private String description;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Category(long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+    public Category() {
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/trilha/back/financys/Entry.java
@@ -0,0 +1,104 @@
+package com.trilha.back.financys;
+
+public class Entry {
+    private long id;
+    private long categoryId;
+    private String name;
+    private String description;
+    private String type;
+    private String amount;
+    private String date;
+    private boolean paid;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public boolean getPaid() {
+        return paid;
+    }
+
+    public void setPaid(boolean paid) {
+        this.paid = paid;
+    }
+
+    public Entry(long id, long categoryId, String name, String description, String type, String amount, String date, boolean paid) {
+        this.id = id;
+        this.categoryId = categoryId;
+        this.name = name;
+        this.description = description;
+        this.type = type;
+        this.amount = amount;
+        this.date = date;
+        this.paid = paid;
+    }
+
+    public Entry() { }
+
+    @Override
+    public String toString() {
+        return "Entry{" +
+                "id=" + id +
+                ", categoryId=" + categoryId +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", type='" + type + '\'' +
+                ", amount='" + amount + '\'' +
+                ", date='" + date + '\'' +
+                ", paid=" + paid +
+                '}';
+    }
+
+}
